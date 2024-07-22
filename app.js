5f8a97de3811af6e68afa89267c8f8df117a11f4
Jeremias <jeremontoya155@gmail.com>
2024-07-22 15:02:55 -0300

    Cambios del Serviciosds con laolddadasdsdasdas

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Para manejar solicitudes JSON
 
 // Middleware para servir archivos estáticos
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('login');
-});
 app.use(session({
   store: new pgSession({
     pool: pool,                // Conexión de la base de datos
@@ -58,16 +56,33 @@ app.post('/login', async (req, res) => {
   }
 });
 
+// Ruta para obtener las sucursales únicas
+app.get('/sucursales', isAuthenticated, async (req, res) => {
+  try {
+    const result = await pool.query('SELECT DISTINCT sucursales FROM recetas');
+    const sucursales = result.rows.map(row => parseInt(row.sucursales, 10)).sort((a, b) => a - b);
+    res.json(sucursales);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error al obtener las sucursales');
+  }
+});
+
 app.get('/recetas', isAuthenticated, (req, res) => {
   res.render('recetas');
 });
 
 app.post('/recetas', isAuthenticated, async (req, res) => {
   const { startDate, endDate, sucursal } = req.body;
-  const result = await pool.query(`
-    SELECT numero FROM recetas 
-    WHERE fechacreacion BETWEEN $1 AND $2 AND sucursales = $3
-  `, [startDate, endDate, sucursal]);
+  let query = 'SELECT numero FROM recetas WHERE fechacreacion BETWEEN $1 AND $2';
+  const params = [startDate, endDate];
+
+  if (sucursal && sucursal !== 'all') {
+    query += ' AND sucursales = $3';
+    params.push(sucursal);
+  }
+
+  const result = await pool.query(query, params);
 
   const numeros = result.rows.map(r => r.numero);
   const txtContent = numeros.join('\n');
@@ -77,6 +92,26 @@ app.post('/recetas', isAuthenticated, async (req, res) => {
   res.write(txtContent, () => {
     res.end();
   });
+});
+
+// Nueva ruta para obtener la cantidad de recetas en un rango de fechas y sucursal específica
+app.post('/recetas-count', isAuthenticated, async (req, res) => {
+  const { startDate, endDate, sucursal } = req.body;
+  let query = 'SELECT COUNT(*) FROM recetas WHERE fechacreacion BETWEEN $1 AND $2';
+  const params = [startDate, endDate];
+
+  if (sucursal && sucursal !== 'all') {
+    query += ' AND sucursales = $3';
+    params.push(sucursal);
+  }
+
+  try {
+    const result = await pool.query(query, params);
+    res.json({ count: result.rows[0].count });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error al obtener la cantidad de recetas');
+  }
 });
 
 // Ruta para cerrar sesión
@@ -93,3 +128,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
